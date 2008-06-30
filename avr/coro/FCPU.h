2c639099bef35c7fb5bf9beb166df2932178631b
scuzzy <scuzzy@29a3e760-5942-0410-98f8-abf4fe8edc1f>
2008-06-30 13:22:57 +0000

    removed unessesary files
    git-svn-id: http://bitrandom.svnrepository.com/svn/micro@33 29a3e760-5942-0410-98f8-abf4fe8edc1f

--- avr/coro/FCPU.h
+++
@@ -1,3 +0,0 @@
-#ifndef F_CPU
-#define F_CPU        20000000UL               		// 20MHz processor
-#endif
--- avr/coro/uart.h
+++
@@ -1,9 +0,0 @@
-#include "FCPU.h"
-#include <avr/interrupt.h>
-
-#define SET_BAUD(baudRate) (((F_CPU / baudRate) / 16L) - 1);
-
-void usart_init();
-inline void putChar(uint8_t c);
-void writeLn(char *strn);
-void writeLnLen(char *data, int len);
