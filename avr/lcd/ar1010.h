23878a1bb25dd43c97c066d8032b0f20bf321e56
scuzzy <scuzzy@29a3e760-5942-0410-98f8-abf4fe8edc1f>
2010-04-20 19:45:16 +0000

    I think I'm almost ready
    git-svn-id: http://bitrandom.svnrepository.com/svn/micro@147 29a3e760-5942-0410-98f8-abf4fe8edc1f

--- avr/lcd/ar1010.h
+++ avr/lcd/ar1010.h
@@ -1,12 +1,30 @@
-/* 
- * File:   ar1010.h
- * Author: scuzzy
+/**
+ *  Copyright (c) 2010 Daine Mamacos <daine.mamacos@reverseorder.net>
  *
- * Created on 17 April 2010, 22:40
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef _AR1010_H
-#define	_AR1010_H
+#ifndef AR1010_H
+#define	AR1010_H
 
 #include <inttypes.h>
 
@@ -31,5 +49,6 @@ static uint16_t getRegister(uint8_t regNumber, uint8_t vola);
 static uint8_t setRegister(uint8_t regNumber, uint16_t regValue);
 static void setAllRegs(uint16_t *regVals);
 static void getCurFreq();
-#endif	/* _AR1010_H */
+
+#endif
 
