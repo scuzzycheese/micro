d708002abece74de6bbc7f75414ac66418989d1f
scuzzy <scuzzy@29a3e760-5942-0410-98f8-abf4fe8edc1f>
2010-10-13 21:33:16 +0000

    small change to the light status
    git-svn-id: http://bitrandom.svnrepository.com/svn/micro@224 29a3e760-5942-0410-98f8-abf4fe8edc1f

--- avr/ethernet/pages/index.c
+++ avr/ethernet/pages/index.c
@@ -6,14 +6,24 @@ int indexPage(struct argData *args, coStData *regs)
 {
 	static int light = 0;
 
+	light ^= 1;
+
 	fib_send("<h1>Atmega 644 webserver</h1>", regs);
 	fib_send("<b>Stats:</b><br/>", regs);
-	fib_send("\tCHIP: Atmel Atmega 644 20Mhz 4Kb RAM.<br />", regs);
-	fib_send("\tNIC: Microchip ENC28J60.<br />", regs);
-	fib_send("\tTCP/IP stack: uIP<br />", regs);
-	fib_send("\tIP: 192.168.0.40<br />", regs);
+	fib_send("\tCHIP: Atmel Atmega 644 20Mhz 4Kb RAM.<br/>", regs);
+	fib_send("\tNIC: Microchip ENC28J60.<br/>", regs);
+	fib_send("\tTCP/IP stack: uIP<br/>", regs);
+	fib_send("\tIP: 192.168.0.40<br/>", regs);
+	if(light)
+	{
+		fib_send("\tLIGHT: On<br/>", regs);
+	}
+	else
+	{
+		fib_send("\tLIGHT: Off<br/>", regs);
+	}
 
-	fib_send("\tARGS: <br />", regs);
+	fib_send("\tARGS: <br/>", regs);
 	int i;
 	for(i = 0; i < 5; i ++)
 	{
@@ -23,7 +33,7 @@ int indexPage(struct argData *args, coStData *regs)
 			fib_send(args[i].argName, regs);
 			fib_send(": ", regs);
 			if(args[i].argValue) fib_send(args[i].argValue, regs);
-			fib_send("<br />", regs);
+			fib_send("<br/>", regs);
 		}
 	}
 
